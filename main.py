389d6b4cf5e26991a33ca36eb0f73ba5d09bca3b
abbfelarb <felicia.arborelius@abbgymnasiet.se>
2024-11-19 08:51:02 +0100

    routes for html and js

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 api_version = "/api/v0"
 
-@app.route(f"{api_version}/get_response", methods = ['POST'])
+
+@app.route(f"{api_version}/get_response", methods=["POST"])
 def get_response():
     data = request.form
     print(data)
@@ -21,35 +22,30 @@ def get_response():
 
     return json.dumps({"id": id.__str__(), "chat_messages": chat_messages})
 
+
 @app.route(f"{api_version}/get_sound/<id>")
 def get_sound(id):
     path = f"./temp/talsyntes_{id}.wav"
 
-
-    
     return send_file(
-         path, 
-         mimetype="audio/wav", 
-         )
+        path,
+        mimetype="audio/wav",
+    )
 
 
+@app.route("/")
+def index():
+
+    f = open("index.html", "rb")
+    return f.read()
 
 
+@app.route("/app.js")
+def js():
 
-
-
-
-
-
-
-
-
-
+    f = open("app.js", "rb")
+    return f.read()
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0.", "8000", debug=True)
-
-
-    
-    
+    app.run("0.0.0.0", "8000", debug=True)
